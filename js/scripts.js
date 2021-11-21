109351457a40046eecf3586fa304bff7ab622c8e
Esther-Anyona <jkemuntoe@gmail.com>
2021-11-21 23:45:13 +0300

    Add jQuery functionalty for the form

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+  $('for#form').on('submit', function(event){
+    event.preventDefault();
+  }
+  // $(".toggle-img").on("click", function(){
+  //   $("toggle.text").toggle();
+  // }); 
+  // $(".form").click(submit(){
+  // let name= $("input#user").val();
+  // let email= $("input#mail").val();
+  // let message= $("textarea#msg").val();
+  if $("input#user").val() && $("input#mail").val();
+    alert("Thank you" + " " + name + " " + "for contacting us. We'll get back to you soon!");
+    }
+ });
