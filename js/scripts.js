491a710d7ae1958690d38f20ab851102c04e57c0
Esther-Anyona <jkemuntoe@gmail.com>
2021-11-22 10:59:05 +0300

    Ensure hover function on portfolio images is achieved

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,31 @@
 // Business Logic
 $(document).ready(function(){
+  $('.card').hover(function(){
+    $('.pjName',this).toggle('1000');
+  }, (function(){
+    $('.pjName',this).toggle('1000');
+    }));
+ 
 
 
-
-
+  // ((name===value) && (email===value) && (message===value));
   // UI Logic
-  $('#form').submit(function(){
+  $('#form').submit(function(e){
+    e.preventDefault();
     let name= $('input#user').val();
     let email= $("input#mail").val();
     let message= $("textarea#msg").val();
+
     // form input validation
+
     if ((name =="") || (email =="") || (message=="")){
       alert("Please fill all the fields in the form");
-      return false;
+      // return false;
     }
-    // else((name===value) && (email===value) && (message===value));{
-    //   alert("Thank you" + " " + name + " " + "for contacting Delani Studio. We'll get back to you soon!");
-    //   return true;
-    // };
+    else {
+      alert("Thank you" + " " + name + " " + "for contacting Delani Studio. We'll get back to you soon!");
+      // return true;
+    }
     
   });
 });
